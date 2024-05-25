52103353eac6e228a8832b185a44943cda6c14dc
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-25 16:42:01 -0300

    Binding unidireccional en TABLE, iteraciones con for de array creado expedienteList

--- src/app/components/expediente/expediente.component.ts
+++ src/app/components/expediente/expediente.component.ts
@@ -11,18 +11,22 @@ import { FormsModule } from '@angular/forms';
 })
 export class ExpedienteComponent {
   expediente: Expediente; //expediente es un objeto que trae los atributos de la clase models
-  listExpediente: Array<Expediente>; //listExpediente es un array contiene el objeto Expediente
+  expedienteList: Array<Expediente>; //expedienteList es un array contiene el objeto Expediente
 
 
   constructor(){
     this.expediente = new Expediente();
-    this.listExpediente = new Array<Expediente>();
+    this.expedienteList = new Array<Expediente>();
   }
 
   registrar(): void {
     console.log(this.expediente);
-    this.listExpediente.push(this.expediente); //EL push agrega un elemento al array, agrega el expediente
+    this.expedienteList.push(this.expediente); //EL push agrega un elemento al array, agrega el expediente, 
+    // expedienteList queda en memoria de expediente, referenciado(this.variable-objeto) por la primera posicion del array
+    // si no pongo esta linea no se guardan los valores
     this.expediente = new Expediente(); //se tiene que reiinicializo para que se rompa la referencia de variable expediente
+    // la referencia expediente apunta a otro objeto en memoria aparte del anterior que se guardo, y va almacenar el nuevo expediente cargado
+    // si no pongo esta ultima linea se sobre escriben los valores cargados, xq no inicializa la variable
   }
 
 }
