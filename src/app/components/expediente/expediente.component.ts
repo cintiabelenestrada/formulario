d8ae93a881b09234d9bcb097f11d0d0f5da148e9
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-25 15:06:28 -0300

    hago el binding de Formulario-se crea metodo, contructor, variable y se lo invoca enel html

--- src/app/components/expediente/expediente.component.ts
+++ src/app/components/expediente/expediente.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Expediente } from '../../models/expediente';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-expediente',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './expediente.component.html',
   styleUrl: './expediente.component.css'
 })
 export class ExpedienteComponent {
+  expediente: Expediente; //expediente es un objeto que trae los atributos de la clase models
+  listExpediente: Array<Expediente>; //listExpediente es un array contiene el objeto Expediente
+
+
+  constructor(){
+    this.expediente = new Expediente();
+    this.listExpediente = new Array<Expediente>();
+  }
+
+  registrar(): void {
+    console.log(this.expediente);
+    this.listExpediente.push(this.expediente); //EL push agrega un elemento al array, agrega el expediente
+    this.expediente = new Expediente(); //se tiene que reiinicializo para que se rompa la referencia de variable expediente
+  }
 
 }
