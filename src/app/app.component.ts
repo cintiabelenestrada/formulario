36f4e167674737549e4f0728f996d98c51252ca5
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-30 20:41:14 -0300

    se agrego correctamente el componente de noticias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TranslateComponent } from "./components/translate/translate.component";
-import { PortalNoticiasComponent } from './components/portal-noticias/portal-noticias.component';
+import { PortalDeNoticiasComponent } from './components/portalNoticias/portal-de-noticias.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, TranslateComponent,PortalNoticiasComponent]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, TranslateComponent, PortalDeNoticiasComponent]
 })
 export class AppComponent {
   title = 'formulario';
---
+++ src/app/components/portalNoticias/portal-de-noticias.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { InterfaceCategorias } from '../../models/categorias';
+import { interfaceArticles } from '../../models/articles';
+import { PortalNoticiasService } from '../../services/portal-noticias.service';
+
+@Component({
+  selector: 'app-portal-de-noticias',
+  standalone: true,
+  imports: [],
+  templateUrl: './portal-de-noticias.component.html',
+  styleUrl: './portal-de-noticias.component.css'
+})
+export class PortalDeNoticiasComponent {
+  categorias: InterfaceCategorias[] = [];
+
+  homepageArticles: interfaceArticles[]= [];
+
+  constructor(private portalNoticias: PortalNoticiasService) {}
+  mostrarDatosDeApi() {
+    this.portalNoticias.getObtenerNoticias().subscribe(
+      (data: any) => {
+        // Categorias de noticias
+        this.categorias = data.categories;
+        console.log('data.categories: ', JSON.stringify(data.categories));
+
+        // Noticias
+        this.homepageArticles = data.homepageArticles;
+        console.log('data.articles: ', JSON.stringify(data.homepageArticles));
+      },
+      (error:any) => {
+        console.log(error);
+      }
+    );
+  }
+}
