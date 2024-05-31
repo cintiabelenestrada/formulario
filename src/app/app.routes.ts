e13cb918bcf5114c42214c23eda29dbfe722f2e2
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-30 21:12:00 -0300

    vv

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
 import { TranslateComponent } from './components/translate/translate.component';
 import { PortalDeNoticiasComponent } from './components/portalNoticias/portal-de-noticias.component';
+import { LayoutPageComponent } from './layouts/layout-page/layout-page.component';
 
 export const routes: Routes = [
+ {
+        path: '',
+        component: LayoutPageComponent,
+        children: [
+    { path: 'traductor', component: TranslateComponent,  title: 'Traductor'},
 
-    {
-        path: 'translate', 
-        component: TranslateComponent,
-    },
-    {
-        path: 'noticias', 
-        component: PortalDeNoticiasComponent,
-    }
+    { path: 'noticias', component: PortalDeNoticiasComponent,title: 'Últimas noticias'},
+    
+    { path: '**', redirectTo: 'traductor', pathMatch: 'full' },
+    ],
+ },
 ];
